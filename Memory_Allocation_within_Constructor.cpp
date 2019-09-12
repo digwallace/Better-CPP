08b50089f679dd9bf5d415380db602f2720218dc
gyiyg <digwallace@gmail.com>
2019-09-12 13:50:20 -0400

    Safely allocate memory in constructor.

--- Memory_Allocation_within_Constructor.cpp
+++ Memory_Allocation_within_Constructor.cpp
@@ -1,13 +1,57 @@
+#include<memory>    // Used for unique_ptr.
 
-class A{
+class DummyClass{};
+
+// Note: Must be compiled using a c++11 compiler.
+//       (i.e. You may need to use the -std=c++11 flag.)
+class ConstructorAllocatingClass{
 
     public:
-        A(){}
+        ConstructorAllocatingClass(DummyClass copy); 
+    private:
+        // Incorrect vs. Correct manner of handling.
+        DummyClass incorrect;
+        std::unique_ptr<DummyClass> correct;
+};
+
+ConstructorAllocatingClass::ConstructorAllocatingClass(DummyClass copy){
+        incorrect = copy;                   // Leaks memory if object fails.
+        correct  = std::unique_ptr<DummyClass>(new DummyClass(copy));   // Deletes object when out of scope.
+}
+
+// ************************ OLD METHOD *****************************
+// Pre-C++11 Method using a Try-Catch.
+class OldMethodClass{
+
+    public:
+        OldMethodClass(DummyClass copy);
+    private:
+        DummyClass  old_method_object;
 
 };
 
+OldMethodClass::OldMethodClass(DummyClass copy){
+
+    old_method_object = copy;
+    if(&old_method_object != &copy){
+        throw "Failed to copy!";
+    }
+
+}
+
+// ********************* END OF OLD METHOD *************************
+
 int main(){
 
+    DummyClass                 class_to_copy;
+    ConstructorAllocatingClass example(class_to_copy);
+
+    // Old Method.
+    try{
+        OldMethodClass              old_example(class_to_copy);    
+    }catch(const char* msg){
+
+    }  // End of Old Method.
 
     return 0;
 }
